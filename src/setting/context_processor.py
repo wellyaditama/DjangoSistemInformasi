34b06f537f3b45502fbc8ee5d0b93fb298ddfc0d
wellyaditama <58581261+wellyaditama@users.noreply.github.com>
2023-12-11 19:49:06 +0700

    Initial Commit

---
+++ src/setting/context_processor.py
@@ -0,0 +1,23 @@
+"""
+Author: Anthony C. Emmanuel
+Title: Django Project Starter Template
+Version: 1.0.0
+Language: Python 3.6
+Date: 07-12-2018
+License: 3-clause BSD
+"""
+
+from django.utils.functional import SimpleLazyObject
+from setting.models import Setting
+import datetime
+
+def setting(request):
+    """Context processor function for global template content display."""
+    if Setting.objects.filter(id=1):
+        data = Setting.objects.filter(id=1)[0]
+        data.year = str(datetime.date.today())[:4]
+    else:
+        data = ''
+    #print(data.year, data.site_title, data.site_phone)
+
+    return {'global_settings':data, 'request':request}
